0de95febaec6f765ba51db665f165e349a183761
mdonze <mdonze@gmail.com>
2023-05-23 10:42:29 -0700

    Defined game object classes

--- breakout.py
+++ breakout.py
@@ -15,6 +15,46 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 # Setup the clock
 clock = pygame.time.Clock()
 
+# Define classes (game objects)
+class Paddle:
+    def __init__(self, x, y, w, h, speed):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.speed = speed
+
+    def move_left(self):
+        self.x += self.speed
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 255, 255), pygame.Rect(self.x, self.y, self.w, self.h))
+
+class Ball:
+    def __init__(self, x, y, radius, speed, direction):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.speed = speed
+        self.direction = direction # A tuple(dx, dy)
+    
+    def move(self):
+        self.x += self.speed * self.direction[0]
+        self.y += self.speed * self.direction[1]
+
+    def draw(self, window):
+        pygame.draw.circle(window, (255, 255, 255), (self.x, self.y), self.radius)
+
+class Brick:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 255, 255), pygame.Rect(self.x, self.y, self.w, self.h))
+
 # Main game loop
 while True:
     # Event handling
